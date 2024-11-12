bf2bc3525cf4cab28e82366b908f24970f1c962b
Nicodeme <nicodemetraore18@gmail.com>
2024-11-12 01:21:04 +0100

    setup of springboot rest project and database connect

---
+++ src/main/java/com/centreMuraz/payroll/employeepayrollSystem/EmployeepayrollSystemApplication.java
@@ -0,0 +1,13 @@
+package com.centreMuraz.payroll.employeepayrollSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeepayrollSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeepayrollSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/centreMuraz/payroll/employeepayrollSystem/EmployeepayrollSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centreMuraz.payroll.employeepayrollSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeepayrollSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
